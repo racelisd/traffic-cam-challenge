c8bdf31ccea19316792c74bf6e348bc7865616fc
racelisd <racelisd@gmail.com>
2014-12-12 11:11:52 -0800

    new changes
    added the bonus window-close option

--- js/app.js
+++ js/app.js
@@ -1,13 +1,42 @@
-// List of Seattle Traffic Cameras
-// http://data.seattle.gov/resource/65fc-btcc.json
 
 "use strict";
 
-//put your code here to create the map, fetch the list of traffic cameras
-//and add them as markers on the map
-//when a user clicks on a marker, you should pan the map so that the marker
-//is at the center, and open an InfoWindow that displays the latest camera
-//image
-//you should also write the code to filter the set of markers when the user
-//types a search phrase into the search box
+$(document).ready(function(){
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 47.6, lng:-122.3},
+		zoom: 12
+	});
 
+	var infoWindow = new google.maps.InfoWindow();
+	
+	$.getJSON("http://data.seattle.gov/resource/65fc-btcc.json", function(mapData){
+			mapData.forEach(function(cam){
+				var mapMarker = new google.maps.Marker( {
+					position: {lat:Number(cam.location.latitude), lng:Number(cam.location.longitude)},
+					map: map,
+				});
+
+				google.maps.event.addListener(mapMarker, 'click', function(){
+					map.panTo(mapMarker.getPosition());
+					infoWindow.open(map, mapMarker);
+					infoWindow.setContent('<h3>' + cam.cameralabel + '</h3> <img src="' + cam.imageurl.url + '"/>');
+				});
+
+				$('#search').bind('search keyup', function() {
+					var elem1 = cam.cameralabel.toLowerCase();
+					var elem2 = $('#search').val().toLowerCase();
+					if(elem1.indexOf(elem2) >= 0){
+						mapMarker.setMap(map);
+					}else{
+						mapMarker.setMap(null);
+					}
+				});
+			})
+	});
+
+	//clicks out of the infoWindow for the camera!
+
+	google.maps.event.addListener(map, 'click', function() {
+        infoWindow.close();
+    });
+});
